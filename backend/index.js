9eedcbad1b86fc78a7049d6e8e147de9c027c7ef
Teejal <teejallohana@gmail.com>
2026-06-26 00:51:03 +0500

    Restore vercel.json

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const productRoutes = require('../routes/productRoutes');
+const authRoute = require('../routes/authroute');
+
+const app = express();
+
+// 1. MIDDLEWARES 
+app.use(cors({ origin: '*' }));
+app.use(express.json()); 
+
+// 2. ROUTES 
+app.use('/api/products', productRoutes);
+app.use('/api/auth', authRoute);
+
+// Test Routes
+app.get('/test', (req, res) => res.json({ ok: true }));
+app.get('/', (req, res) => {
+  res.json({ message: 'Backend is running!' });
+});
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB Connected!'))
+  .catch(err => console.log('Error:', err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
