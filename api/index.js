ff07d2255e0cf3d486d66e2df4b6e405dba6c21a
Teejal <teejallohana@gmail.com>
2026-06-26 03:04:55 +0500

    fix: add server listen

--- api/index.js
+++ api/index.js
@@ -27,4 +27,6 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected!'))
   .catch(err => console.log('Error:', err));
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 module.exports = app;
